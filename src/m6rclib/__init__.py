c7a3625f1541aaa24b42aa622bc561a9e663f1f3
Dave Hudson <dave-hudson@users.noreply.github.com>
2024-11-19 09:49:33 +0000

    Expose missing API class
    * Expose an API class that should have been public.

--- src/m6rclib/__init__.py
+++ src/m6rclib/__init__.py
@@ -4,7 +4,7 @@ __version__ = "0.4.1"
 
 # Export main classes so users can import directly from m6rclib
 from .metaphor_ast_node import MetaphorASTNode, MetaphorASTNodeType
-from .metaphor_parser import MetaphorParser, MetaphorParserError
+from .metaphor_parser import MetaphorParser, MetaphorParserError, MetaphorParserSyntaxError
 from .metaphor_formatters import format_ast, format_errors
 
 # List what should be available when using `from m6rclib import *`
@@ -13,6 +13,7 @@ __all__ = [
     "MetaphorASTNodeType",
     "MetaphorParser",
     "MetaphorParserError",
+    "MetaphorParserSyntaxError",
     "format_ast",
     "format_errors"
 ]
